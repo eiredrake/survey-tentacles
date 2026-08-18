c3a3447987c5caf189f84baac11f45941c9bff40
eiredrake <eiredrake@gmail.com>
2026-08-18 10:03:22 -0400

    Initial project structure

---
+++ backend/src/main/java/org/eiredrake/tentacles/TentaclesApplication.java
@@ -0,0 +1,13 @@
+package org.eiredrake.tentacles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentaclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentaclesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/eiredrake/tentacles/TentaclesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eiredrake.tentacles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentaclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
