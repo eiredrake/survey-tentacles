61dbcb4e0f2d1587b3caa4d5b411c1f2f985348a
eiredrake <eiredrake@gmail.com>
2026-08-18 22:28:58 -0400

    additional backend changes.

---
+++ backend/src/main/java/org/eiredrake/tentacles/controller/ApiExceptionHandler.java
@@ -0,0 +1,22 @@
+package org.eiredrake.tentacles.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, Object> handleIllegalArgumentException(
+            IllegalArgumentException ex) {
+
+        return Map.of(
+            "error", ex.getMessage()
+        );
+    }
+}
